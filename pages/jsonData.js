436833e9e9104333a87dced167484400a6ab0729
Pushparaj Singh <pushparajsingh448@gmail.com>
2022-12-05 14:46:44 +0530

    next js quiz contest

---
+++ pages/jsonData.js
@@ -0,0 +1,103 @@
+export const data = [
+  {
+    Question: "Q.1 2 + 3 + 55 = ?",
+    Option: [
+      { Option: "60", Check: true },
+      { Option: "62", Check: false },
+      { Option: "60.5", Check: false },
+      { Option: "80", Check: false },
+    ],
+  },
+
+  {
+    Question: "Q.2 2 * 3 + 55 - 2 = ?",
+    Option: [
+      { Option: "50", Check: false },
+      { Option: "59", Check: true },
+      { Option: "50.5", Check: false },
+      { Option: "60", Check: false },
+    ],
+  },
+  {
+    Question: "Q.3 2 * 3 + 5 + (55 - 2) * 2 = ?",
+    Option: [
+      { Option: "101", Check: false },
+      { Option: "117", Check: true },
+      { Option: "112", Check: false },
+      { Option: "119", Check: false },
+    ],
+  },
+  {
+    Question: "Q.4 2 * 3 ** 2 + 6 = ?",
+    Option: [
+      { Option: "25", Check: false },
+      { Option: "24", Check: true },
+      { Option: "20", Check: false },
+      { Option: "21", Check: false },
+    ],
+  },
+  {
+    Question: "Q.5 What is 33 x 7?",
+    Option: [
+      { Option: "238", Check: false },
+      { Option: "231", Check: true },
+      { Option: "225", Check: false },
+      { Option: "233", Check: false },
+    ],
+  },
+  {
+    Question: "Q.6 Find the value of x, 25 + x = 5",
+    Option: [
+      { Option: "-23", Check: false },
+      { Option: "20", Check: false },
+      { Option: "28", Check: false },
+      { Option: "-20", Check: true },
+    ],
+  },
+  {
+    Question: "Q.7 if x equal to 10,Find y ?,  x + y + 10 = ?",
+    Option: [
+      { Option: "-20", Check: true },
+      { Option: "22", Check: false },
+      { Option: "28", Check: false },
+      { Option: "20", Check: false },
+    ],
+  },
+  {
+    Question: "Q.8 x+y+z = 100,if x = 10, z = 30, Find y?",
+    Option: [
+      { Option: "-20", Check: false },
+      { Option: "60", Check: true },
+      { Option: "28", Check: false },
+      { Option: "19", Check: false },
+    ],
+  },
+  {
+    Question: "Q.9 (2 + 5) * 2 = ?",
+    Option: [
+      { Option: "15", Check: false },
+      { Option: "20", Check: false },
+      { Option: "14", Check: true },
+      { Option: "10", Check: false },
+    ],
+  },
+  {
+    Question: "Q.10 (x+5)**2=?,(a+b)**2=a**2+b**2+2ab",
+    Option: [
+      { Option: "x**2 + 25", Check: false },
+      { Option: "x**3 + 25 + 10x", Check: false },
+      { Option: "x**2 + 10x", Check: false },
+      { Option: "x**2 + 25 + 10x", Check: true },
+    ],
+  },
+];
+
+// {
+//     Question: "Q.1 2*3+55-2 = ?",
+//     Option: [
+//       { Option1: "50", Check: true },
+//       { Option2: "59", Check: true },
+//       { Option3: "50.5", Check: true },
+//       { Option4: "60", Check: true },
+//     ],
+//   },
